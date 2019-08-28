a7050302565d6d8bcc099c41ff9ef32f969733b5
tamirissg <tamiris@icamiabas.com.br>
2019-08-27 23:15:54 -0300

    alteração na cifra de césar

--- src/cipher.js
+++ src/cipher.js
@@ -2,36 +2,53 @@
 window.cipher = {
   encode:encode,
   decode:decode
-}
+};
 
 function encode (offset,string){
   let arrayVazia = [];
   let tamanho = string.length;
   for(let i=0; i<tamanho; i++){
-    let somando = ((string[i].charCodeAt(0) - 65 + offset) % 26) + 65;
+  let codigoDeletraAsc = string[i].charCodeAt(0);
+  if(codigoDeletraAsc >=65 && codigoDeletraAsc <=90){
+    let somando = (codigoDeletraAsc - 65 + offset) % 26 + 65;
     let soma = String.fromCharCode(somando);
     arrayVazia.push(soma);
-  
+}else if (codigoDeletraAsc >=97 && codigoDeletraAsc <=122){
+let somando = (codigoDeletraAsc - 97 + offset) % 26 + 97;
+let soma = String.fromCharCode(somando);
+arrayVazia.push(soma);
+  } else{
+    arrayVazia.push(string[i]);
   }
-  return arrayVazia.join("");
-
+    
 }
-  
 
-
+  return arrayVazia.join(""); 
+    }
+ 
 
 function decode(offset,string){
   let arrayVazia = [];
   let tamanho = string.length;
-  // let array = Array.from(string);
   for(let i=0; i<tamanho; i++){
-    let somando = ((string[i].charCodeAt(0) + 65 - offset) % 26) - 65;
+  let codigoDeletraAsc = string[i].charCodeAt(0);
+  if(codigoDeletraAsc >=65 && codigoDeletraAsc <=90){
+    let somando = (codigoDeletraAsc - 90 - offset) % 26 + 90;
     let soma = String.fromCharCode(somando);
     arrayVazia.push(soma);
-  
+}else if (codigoDeletraAsc >=97 && codigoDeletraAsc <=122){
+let somando = (codigoDeletraAsc - 122 - offset) % 26 + 122;
+let soma = String.fromCharCode(somando);
+arrayVazia.push(soma);
+  } else{
+    arrayVazia.push(string[i]);
   }
-  return arrayVazia.join("");
-
+    
 }
+
+  return arrayVazia.join(""); 
+    }
+
+
 
 
--- src/index.js
+++ src/index.js
@@ -1,24 +1,30 @@
 
+
+
 function encode (){
 event.preventDefault()
 let offset = parseInt(document.getElementById("offset").value);
 let string = document.getElementById("string").value;
 console.log(string)
 let resultado = window.cipher.encode(offset,string);
-console.log(resultado);
-document.getElementById("resultado1").innerHTML = `<p> Suma mensagem Codificada é ${resultado} </p>`;
+document.getElementById("resultado1").innerHTML = `<p> Sua mensagem Codificada é ${resultado} </p>`;
 }
 
 
-
-
-function Decode(){
+function decode(){
     event.preventDefault()
     let offset = parseInt(document.getElementById("offset2").value);
     let string = document.getElementById("string2").value;
-    let soma  = window.cipher.decode(offset,string);
-    console.log(soma);
-    document.getElementById("resultado2").innerHTML = `<p> Suma mensagem Codificada é ${soma} </p>`;
+    let resultado  = window.cipher.decode(offset,string);
+   document.getElementById("resultado2").innerHTML = `<p> Sua mensagem Codificada é ${resultado} </p>`;
 };
    
+
+document.getElementById("myBtn").addEventListener("click", encode)
+document.getElementById("myBtn2").addEventListener("click", decode)
+document.getElementById("myBtn3").addEventListener("click", "")
+document.getElementById("myBtn4").addEventListener("click", "")
+
+
+
 
