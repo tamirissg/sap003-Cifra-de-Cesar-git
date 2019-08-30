dc9499984d2f3bb6ed0e6bcedecd2d9e5293b1e5
tamirissg <tamiris@icamiabas.com.br>
2019-08-29 21:49:36 -0300

    validação erros

--- src/index.js
+++ src/index.js
@@ -1,30 +1,22 @@
-
-
-
-function encode (){
-event.preventDefault()
-let offset = parseInt(document.getElementById("offset").value);
-let string = document.getElementById("string").value;
-console.log(string)
-let resultado = window.cipher.encode(offset,string);
-document.getElementById("resultado1").innerHTML = `<p> Sua mensagem Codificada é ${resultado} </p>`;
+function encode() {
+  event.preventDefault()
+  let offset = parseInt(document.getElementById("offset").value);
+  let string = document.getElementById("string").value;
+  let resultado = window.cipher.encode(offset, string);
+  document.getElementById("resultado1").innerHTML = `<p> Sua mensagem Codificada é ${resultado} </p>`;
 }
 
 
-function decode(){
-    event.preventDefault()
-    let offset = parseInt(document.getElementById("offset2").value);
-    let string = document.getElementById("string2").value;
-    let resultado  = window.cipher.decode(offset,string);
-   document.getElementById("resultado2").innerHTML = `<p> Sua mensagem Codificada é ${resultado} </p>`;
-};
-   
+function decode() {
+  event.preventDefault()
+  let offset = parseInt(document.getElementById("offset2").value);
+  let string = document.getElementById("string2").value;
+  let resultado = window.cipher.decode(offset, string);
+  document.getElementById("resultado2").innerHTML = `<p> Sua mensagem Codificada é ${resultado} </p>`;
+}
+
 
 document.getElementById("myBtn").addEventListener("click", encode)
 document.getElementById("myBtn2").addEventListener("click", decode)
 document.getElementById("myBtn3").addEventListener("click", "")
 document.getElementById("myBtn4").addEventListener("click", "")
-
-
-
-
